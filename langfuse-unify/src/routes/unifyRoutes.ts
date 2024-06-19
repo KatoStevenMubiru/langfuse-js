18cc9eaa568a993445e782d7cd87adbb518755ce
Kato Steven Mubiru <kato.steven60@gmail.com>
2024-06-19 11:05:34 +0300

    Delete unifyRoutes.ts

--- langfuse-unify/src/routes/unifyRoutes.ts
+++
@@ -1,8 +0,0 @@
-import { Router } from 'express';
-import { handleQuery } from '../controllers/unifyController';
-
-const router = Router();
-
-router.post('/query', handleQuery);
-
-export default router;
